4b85b9b59bae883305e8e6fb35775f67b9ceecdd
Nicky Ru <nikitkaruban@gmail.com>
2021-09-11 19:14:16 +0800

    Update home.spec.ts

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -5,4 +5,9 @@ describe('Homepage', () => {
   it('should contain Pebble article', function() {
     cy.contains('article', 'Pebble data')
   });
+  context('Pebble article', () => {
+    it('should link to pebble page', function() {
+      cy.contains('a', 'Pebble data').should('have.attr', 'href', '/pebble')
+    });
+  })
 })
