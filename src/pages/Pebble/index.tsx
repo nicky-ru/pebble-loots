12a4ff01f2c38fbbd53746da5a80145b5a2b147e
Nicky Ru <nikitkaruban@gmail.com>
2021-09-11 20:50:02 +0800

    Update index.tsx
    add fetching with pebble subgraph

--- src/pages/Pebble/index.tsx
+++ src/pages/Pebble/index.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import apolloClient from '../../apollo/client';
+import { getApps, getDevices } from '../../graphql/queries';
+import _ from 'lodash';
 import { observer } from 'mobx-react-lite';
 import { Container } from '@chakra-ui/react';
+import gql from 'graphql-tag';
 
 export const Pebble = observer(() => {
+
+  useEffect(() => {
+    async function init_pebble() {
+      const data = await apolloClient.query({
+        query: gql(getApps)
+      })
+      console.log(_.get(data, 'data.applications'));
+    }
+
+    init_pebble();
+  })
+
   return (
     <Container maxW="md">
     </Container>
