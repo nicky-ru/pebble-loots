1fedcdf8061ec213963203a3ad18394e3762cd31
Nicky Ru <nikitkaruban@gmail.com>
2021-09-12 17:29:28 +0800

    Update DevicesList.tsx
    add links

--- src/components/Devices/DevicesList.tsx
+++ src/components/Devices/DevicesList.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import { Container, UnorderedList, ListItem } from '@chakra-ui/react';
+import { Container, UnorderedList, ListItem, Link } from '@chakra-ui/react';
 import { useStore } from '@/store/index';
 
 export const DevicesList = observer(() => {
   const { pebble } = useStore();
 
   return(
-    <Container maxW="container.lg">
+    <Container maxW="container.lg" id={'devices-list'}>
       <UnorderedList>
         {pebble.devices?.map(device => (
-          <ListItem key={device.address}>{device.address}</ListItem>
+          <ListItem listStyleType={'none'} key={device.address}>
+            <Link href={`/devices/${device.address}`}>
+              {device.address}
+            </Link>
+          </ListItem>
         ))}
       </UnorderedList>
     </Container>
