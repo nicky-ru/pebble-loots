b55c315ac2adb16c5255bac7b3394aca7bbf3f70
Nicky Ru <nikitkaruban@gmail.com>
2021-09-12 18:42:53 +0800

    Create records store

---
+++ src/store/rec.ts
@@ -0,0 +1,19 @@
+import { makeAutoObservable } from 'mobx';
+
+class DeviceRecord {
+  imei: "103381234567402"
+  raw: "0x0894b901105618e0e8fa4e20d5e2b1d20528c8c61b38bc9c0940d73a50ff2e5a04811e131a6206821ec20bea186a1031383964386463643766373635633931"
+  signature: "0xe7166f3af88bb1f75d0e54c0faa64cc9274ba6ee8154ffdd5083babd135f8d1e176ad488251f7839739c126f1c45403b3be0fbdaa433fc517031d4bcf18f3a271c"
+}
+
+export class RecordStore {
+  records = Array<DeviceRecord>();
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setRecords(records: Array<DeviceRecord>) {
+    this.records = records;
+  }
+}
--- src/store/root.ts
+++ src/store/root.ts
@@ -2,10 +2,12 @@ import { LangStore } from './lang';
 import { GodStore } from './god';
 import { TokenStore } from './token';
 import { PebbleStore } from './pebble';
+import { RecordStore } from '@/store/rec';
 
 export class RootStore {
   lang = new LangStore();
   god = new GodStore(this);
   token = new TokenStore(this);
   pebble = new PebbleStore();
+  rec = new RecordStore();
 }
