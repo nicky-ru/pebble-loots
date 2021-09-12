b981b475d16ec83a1bc24f13637cc4d858ed04ba
Nicky Ru <nikitkaruban@gmail.com>
2021-09-12 16:59:42 +0800

    Update index.tsx

--- src/pages/Devices/index.tsx
+++ src/pages/Devices/index.tsx
@@ -3,24 +3,27 @@ import apolloClient from '../../apollo/client';
 import { getDevices } from '../../graphql/queries';
 import _ from 'lodash';
 import { observer } from 'mobx-react-lite';
-import { Container } from '@chakra-ui/react';
+import { Container, Text } from '@chakra-ui/react';
 import gql from 'graphql-tag';
+import { useStore } from '@/store/index';
+import { DevicesList } from '@/components/Devices/DevicesList';
 
 export const Devices = observer(() => {
+  const { pebble } = useStore()
 
   useEffect(() => {
     async function init_pebble() {
       const data = await apolloClient.query({
         query: gql(getDevices)
       })
-      console.log(_.get(data, 'data.devices'));
+      pebble.setDevices(_.get(data, 'data.devices'));
     }
-
     init_pebble();
-  })
+  }, [])
 
   return (
-    <Container maxW="md">
+    <Container maxW="container.lg">
+      <DevicesList/>
     </Container>
   );
 });
