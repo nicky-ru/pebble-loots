7a627496e513a565e517a32cbed4b0a01f9981d1
Nicky Ru <nikitkaruban@gmail.com>
2021-09-12 17:01:49 +0800

    Update pebble.ts

--- src/store/pebble.ts
+++ src/store/pebble.ts
@@ -1,7 +1,25 @@
 import { makeAutoObservable } from 'mobx';
 
+class Device {
+  address: "0x94ac89a5da0935bcbfade18762a8c5de75fa8ae3"
+  config: null
+  data: null
+  firmware: ""
+  id: "100000000000001"
+  lastDataTime: null
+  name: "100000000000001"
+  owner: "0x813e92166576b7eaa50dbc985ef61c2e9da13693"
+  state: null
+  status: 2
+}
+
 export class PebbleStore {
+  devices = Array<Device>();
+
   constructor() {
     makeAutoObservable(this);
+  }
+  setDevices(devices: Array<Device>) {
+    this.devices = devices;
   }
 }
