6421d3f08d073f80c97150834c1a18fd5a2a6655
Nicky Ru <nikitkaruban@gmail.com>
2021-09-12 17:24:31 +0800

    Update devices.spec.ts

--- cypress/integration/devices.spec.ts
+++ cypress/integration/devices.spec.ts
@@ -2,6 +2,9 @@ describe('Devices page', () => {
   beforeEach(() => {
     cy.visit('/devices')
   })
+  it('should have title', function() {
+    cy.contains('h1', 'My devices')
+  });
   it('should contain devices list', function() {
     cy.get('li')
   });
