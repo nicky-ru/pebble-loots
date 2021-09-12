cd6a49197e7dda6bfba41eac0bef772bb7027d1f
Nicky Ru <nikitkaruban@gmail.com>
2021-09-12 17:23:41 +0800

    Update index.tsx

--- src/pages/Pebble/index.tsx
+++ src/pages/Pebble/index.tsx
@@ -1,26 +1,7 @@
-import React, { useEffect } from 'react';
-import apolloClient from '../../apollo/client';
-import { getApps, getDevices } from '../../graphql/queries';
-import _ from 'lodash';
+import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { Container } from '@chakra-ui/react';
-import gql from 'graphql-tag';
 
 export const Pebble = observer(() => {
-
-  useEffect(() => {
-    async function init_pebble() {
-      const data = await apolloClient.query({
-        query: gql(getApps)
-      })
-      console.log(_.get(data, 'data.applications'));
-    }
-
-    init_pebble();
-  })
-
-  return (
-    <Container maxW="md">
-    </Container>
-  );
+  return <Container>Here will be dashboard</Container>;
 });
