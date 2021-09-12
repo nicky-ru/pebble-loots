ad92efd8852934fcb48600ee02a10c277d8e0146
Nicky Ru <nikitkaruban@gmail.com>
2021-09-12 17:01:56 +0800

    Update root.ts

--- src/store/root.ts
+++ src/store/root.ts
@@ -1,9 +1,11 @@
 import { LangStore } from './lang';
 import { GodStore } from './god';
 import { TokenStore } from './token';
+import { PebbleStore } from './pebble';
 
 export class RootStore {
   lang = new LangStore();
   god = new GodStore(this);
   token = new TokenStore(this);
+  pebble = new PebbleStore();
 }
