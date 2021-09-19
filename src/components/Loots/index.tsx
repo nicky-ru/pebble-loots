bf8082c0bef4e360f283cafe8aaf0d3c529b06e9
Nicky Ru <nikitkaruban@gmail.com>
2021-09-19 15:13:10 +0800

    Create index.tsx

---
+++ src/components/Loots/index.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { observer } from 'mobx-react-lite';
+import { Container, Text } from '@chakra-ui/react';
+import { useStore } from '@/store/index';
+
+export const LootCards = observer(() => {
+
+  return(
+    <Container>
+    </Container>
+  );
+});
