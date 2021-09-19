6a4602e321434dc992a7b4528a1112be28f93cef
Nicky Ru <nikitkaruban@gmail.com>
2021-09-19 18:10:39 +0800

    Update index.tsx

--- src/components/Loots/index.tsx
+++ src/components/Loots/index.tsx
@@ -4,9 +4,25 @@ import { Container, Text } from '@chakra-ui/react';
 import { useStore } from '@/store/index';
 
 export const LootCards = observer(() => {
+  const { god, ploot } = useStore();
+
+  useEffect(() => {
+    loadLoots();
+  })
+
+  async function loadLoots() {
+    const _balance = await ploot.contract.balanceOf(god.currentNetwork.account);
+    ploot.setBalance(_balance.toNumber());
+
+    console.log("calfl", _balance);
+    // const tokenUri = await lootContract.tokenURI(1);
+    // const meta = await axios.get(tokenUri);
+    // console.log(meta.data.image)
+  }
 
   return(
     <Container>
+      My balance is {ploot.balance}
     </Container>
   );
 });
