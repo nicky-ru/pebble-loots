cc536b3a265e69be9f38da0f17f97d6d7ec74eae
Nicky Ru <nikitkaruban@gmail.com>
2021-09-20 18:41:08 +0800

    Update index.tsx

--- src/components/Loots/index.tsx
+++ src/components/Loots/index.tsx
@@ -1,19 +1,30 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { observer } from 'mobx-react-lite';
-import { Container, Text } from '@chakra-ui/react';
-import { useStore } from '@/store/index';
-import { TokenState } from '@/store/lib/TokenState';
+import { Container, Box, Heading, Wrap, WrapItem } from '@chakra-ui/react';
 
 interface PropsType {
-  bal: number;
+  balance: number;
   name: string;
   symbol: string;
+  tokenUris: any[];
 }
 
 export const LootCards = observer((props: PropsType) => {
   return(
-    <Container>
-      My balance is {props.bal} name is {props.name} symbol is {props.symbol}
+    <Container textAlign={"center"} maxW={'full'}>
+      <Heading mb={4}>
+        My balance is {props.balance} name is {props.name} symbol is {props.symbol}
+      </Heading>
+      <Wrap spacing="30px" justify="center">
+        {props.tokenUris?.map(uri => (
+          <WrapItem key={uri.data.name}>
+            <Box w={"350px"} h={"350px"}>
+              <img src={uri.data.image}/>
+              {uri.data.name}
+            </Box>
+          </WrapItem>
+        ))}
+      </Wrap>
     </Container>
   );
 });
