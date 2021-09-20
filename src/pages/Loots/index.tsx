4dc72bcadf777b3b0a3657c057344a2c0005051c
Nicky Ru <nikitkaruban@gmail.com>
2021-09-20 18:41:03 +0800

    Update index.tsx

--- src/pages/Loots/index.tsx
+++ src/pages/Loots/index.tsx
@@ -4,6 +4,7 @@ import { Container} from '@chakra-ui/react';
 import { useStore } from '@/store/index';
 import { LootCards } from '@/components/Loots';
 import { TransactionResponse } from '@ethersproject/providers';
+import axios from 'axios';
 
 export const MyLoots = observer(() => {
   const { ploot } = useStore();
@@ -11,8 +12,17 @@ export const MyLoots = observer(() => {
   const observable = useLocalObservable(() => ({
     tokenIds: [],
     chainId: 0,
+    balance: 0,
+    tokenUris: [],
     setChainId(newChainId: number) {
       this.chainId = newChainId;
+    },
+    setBalance(newBalance: Partial<TransactionResponse>) {
+      // @ts-ignore
+      this.balance = newBalance.toNumber();
+    },
+    setTokenUris(newUris) {
+      this.tokenUris = newUris;
     }
   }))
 
@@ -22,12 +32,44 @@ export const MyLoots = observer(() => {
     }
   }, [ploot.god.currentChain.chainId]);
 
+  useEffect(() => {
+    if (observable.chainId) {
+      updateBalance();
+    }
+  }, [observable.chainId])
+
+  useEffect(() => {
+    if (observable.balance) {
+      fetchLoots();
+    }
+  }, [observable.balance]);
+
+  async function fetchLoots() {
+    const tokenIds = Array(observable.balance);
+
+    for (let i = 0; i < observable.balance; i++) {
+      tokenIds[i] = await ploot.contracts[observable.chainId].tokenOfOwnerByIndex({params: [ploot.god.currentNetwork.account, i]})
+    }
+    const tokenUris = await Promise.all(tokenIds.map(async (tid) => {
+      const uri = await ploot.contracts[observable.chainId].getTokenUri({params: [tid.toNumber()]});
+      return await axios.get(uri.toString());
+    }))
+
+    observable.setTokenUris(tokenUris);
+  }
+
+  async function updateBalance() {
+    const balance = await ploot.contracts[observable.chainId].balanceOf({params: [ploot.god.currentNetwork.account]});
+    observable.setBalance(balance);
+  }
+
   return(
-    <Container>
+    <Container maxW={'container.xl'} mt={10}>
       <LootCards
-        bal={0}
+        balance={observable.balance}
         name={observable.chainId? ploot.contracts[observable.chainId].name : ""}
         symbol={observable.chainId? ploot.contracts[observable.chainId].symbol : ""}
+        tokenUris={observable.tokenUris}
       />
     </Container>
   );
