482f6d5f8716d9b23cbbb3ed39eed770afb886b0
Nicky Ru <nikitkaruban@gmail.com>
2021-09-24 19:33:20 +0800

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,7 +10,7 @@ export const Home = observer(() => {
       <SimpleGrid minChildWidth="200px" spacing="10px" py="6">
         {ToolConfig.map((i) => (
           <LinkBox as="article" w="200px" p="4" borderWidth="1px" rounded="md" key={i.name}>
-            <LinkOverlay href={i.path} target="__blank">
+            <LinkOverlay href={i.path} target="_self">
               <Text>{i.name}</Text>
             </LinkOverlay>
             {i.tags && (
