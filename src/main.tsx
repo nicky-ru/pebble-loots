d4b3887ee07537d6b77b177c3944798ad0628f14
Nicky Ru <nikitkaruban@gmail.com>
2021-09-25 16:42:58 +0800

    Update main.tsx
    and brouser router to app root

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import App from './index';
 import { Buffer } from 'buffer';
 (globalThis as any).Buffer = Buffer;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
