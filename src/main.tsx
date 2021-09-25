67405ca842fa14c7f0431de06fea7bc0ab0e0693
Nicky Ru <nikitkaruban@gmail.com>
2021-09-25 17:14:04 +0800

    Squashed commit of the following:
    commit ee81ae0ca3d2e69110f5d900a8a4c18f100e9be6
    Merge: 86728df f3f16d3
    Author: Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
    Date:   Sat Sep 25 17:07:05 2021 +0800
    
        Merge pull request #3 from nicky-ru/hashrouting
    
        Hashrouting
    
    commit f3f16d305e77502d45bdc53f106496906259bab5
    Author: Nicky Ru <nikitkaruban@gmail.com>
    Date:   Sat Sep 25 16:58:39 2021 +0800
    
        use hash routing instead of browser routing
    
    commit 86728dfe3161b281a67f85ea12349df05ab038a2
    Merge: cd72127 3bb061e
    Author: Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
    Date:   Sat Sep 25 11:18:57 2021 +0800
    
        Merge pull request #2 from nicky-ru/dev
    
        Add error boundaries
    
    commit cd7212706433646cc7385b74e6c70a7e225f2d08
    Merge: 66c1a6a 27a0db1
    Author: Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
    Date:   Sat Sep 25 03:16:47 2021 +0800
    
        Merge pull request #1 from nicky-ru/dev
    
        Dev

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from './index';
 import { Buffer } from 'buffer';
 (globalThis as any).Buffer = Buffer;
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
