ee7c77357517df0fe06a75b7654c6497f9f76bd5
Nicky Ru <nikitkaruban@gmail.com>
2021-09-25 21:51:26 +0800

    Update DevicesList.tsx
    Deactivate links

--- src/components/Devices/DevicesList.tsx
+++ src/components/Devices/DevicesList.tsx
@@ -1,34 +1,38 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import { Container, Link, Table, Thead, Tr, Th, Td, Tbody } from '@chakra-ui/react';
-import { Link as ReactLink } from "react-router-dom";
+import { Container, Table, Thead, Tr, Th, Td, Tbody, Box, TableCaption } from '@chakra-ui/react';
 import { useStore } from '@/store/index';
 
 export const DevicesList = observer(() => {
   const { pebble } = useStore();
 
   return(
-    <Container mt={10} maxW="container.lg" id={'devices-list'}>
-      <Table size="sm">
-        <Thead>
-          <Tr>
-            <Th>Id</Th>
-            <Th>Name</Th>
-            <Th>Address</Th>
-            <Th>Owner</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {pebble.devices?.map(device => (
-            <Tr key={device.id}>
-              <Td><Link as={ReactLink} to={`/devices/${device.id}`}>{device.id}</Link></Td>
-              <Td><Link as={ReactLink} to={`/devices/${device.id}`}>{device.name}</Link></Td>
-              <Td><Link as={ReactLink} to={`/devices/${device.id}`}>{device.address}</Link></Td>
-              <Td><Link as={ReactLink} to={`/devices/${device.id}`}>{device.owner}</Link></Td>
+    <Container maxW="container.lg" id={'devices-list'}>
+      <Box>
+        <Table size="sm" variant="striped">
+          <TableCaption placement={"top"}>
+            You can find your device here if you have registered it on IOTT portal
+          </TableCaption>
+          <Thead>
+            <Tr>
+              <Th>IMEI</Th>
+              <Th>Name</Th>
+              {/*<Th>Address</Th>*/}
+              {/*<Th>Owner</Th>*/}
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
+          </Thead>
+          <Tbody>
+            {pebble.devices?.map(device => (
+              <Tr key={device.id}>
+                <Td>{device.id}</Td>
+                <Td>{device.name}</Td>
+                {/*<Td><Link as={ReactLink} to={`/devices/${device.id}`}>{device.address}</Link></Td>*/}
+                {/*<Td><Link as={ReactLink} to={`/devices/${device.id}`}>{device.owner}</Link></Td>*/}
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
     </Container>
     );
 });
