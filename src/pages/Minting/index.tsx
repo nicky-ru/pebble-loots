3bb061ed927c3f2b688417970ec2544a4ca081b6
Nicky Ru <nikitkaruban@gmail.com>
2021-09-25 11:13:22 +0800

    Update index.tsx

--- src/pages/Minting/index.tsx
+++ src/pages/Minting/index.tsx
@@ -3,6 +3,8 @@ import { observer, useLocalObservable } from 'mobx-react-lite';
 import { Container, Input, Button, FormControl, FormLabel, FormHelperText, Heading } from '@chakra-ui/react';
 import { useStore } from '@/store/index';
 import BigNumber from 'bignumber.js';
+import { ErrorFallback } from '@/components/ErrorFallback';
+import { ErrorBoundary } from 'react-error-boundary';
 
 export const Minting = observer(() => {
   const { ploot } = useStore();
@@ -34,26 +36,28 @@ export const Minting = observer(() => {
 
 
   return (
-    <Container textAlign={"center"} maxW={'container.lg'} mt={10}>
-      <Heading>Here you can mint a Loot of your device</Heading>
-      <FormControl id="deviceId">
-        <FormLabel>Device id to mint</FormLabel>
-        <Input
-          type={'text'}
-          placeholder={'Input your device id/imei'}
-          value={observable.tokenId}
-          onChange={(e) => {
-            observable.setTokenId(e.target.value);
-          }}
-        />
-        <FormHelperText>You need to be an owner of the device</FormHelperText>
-        <Button
-          mt={4}
-          onClick={() => {handleClaim()}}
-        >
-          Submit
-        </Button>
-      </FormControl>
-    </Container>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Container textAlign={"center"} maxW={'container.lg'} mt={10}>
+        <Heading>Here you can mint a Loot of your device</Heading>
+        <FormControl id="deviceId">
+          <FormLabel>Device id to mint</FormLabel>
+          <Input
+            type={'text'}
+            placeholder={'Input your device id/imei'}
+            value={observable.tokenId}
+            onChange={(e) => {
+              observable.setTokenId(e.target.value);
+            }}
+          />
+          <FormHelperText>You need to be an owner of the device</FormHelperText>
+          <Button
+            mt={4}
+            onClick={() => {handleClaim()}}
+          >
+            Submit
+          </Button>
+        </FormControl>
+      </Container>
+    </ErrorBoundary>
   );
 });
