c7b18ae5523591be22bc2b91f7d8a01675ebf278
Nicky Ru <nikitkaruban@gmail.com>
2021-09-25 11:08:56 +0800

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,28 +4,32 @@ import { Container, LinkBox, SimpleGrid, LinkOverlay, Stack } from '@chakra-ui/r
 import { Link } from "react-router-dom";
 import { ToolConfig } from '../../config/ToolConfig';
 import { Badge, Text } from '@chakra-ui/layout';
+import { ErrorFallback } from '@/components/ErrorFallback';
+import { ErrorBoundary } from 'react-error-boundary';
 
 export const Home = observer(() => {
   return (
-    <Container maxW="7xl">
-      <SimpleGrid minChildWidth="200px" spacing="10px" py="6">
-        {ToolConfig.map((i) => (
-          <LinkBox as="article" w="200px" p="4" borderWidth="1px" rounded="md" key={i.name}>
-            <LinkOverlay as={Link} to={i.path} target="_self">
-              <Text>{i.name}</Text>
-            </LinkOverlay>
-            {i.tags && (
-              <Stack direction="row" mt="2">
-                {i.tags.map((i) => (
-                  <Badge key={i} variant="outline" colorScheme="green">
-                    {i}
-                  </Badge>
-                ))}
-              </Stack>
-            )}
-          </LinkBox>
-        ))}
-      </SimpleGrid>
-    </Container>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Container maxW="7xl">
+        <SimpleGrid minChildWidth="200px" spacing="10px" py="6">
+          {ToolConfig.map((i) => (
+            <LinkBox as="article" w="200px" p="4" borderWidth="1px" rounded="md" key={i.name}>
+              <LinkOverlay as={Link} to={i.path} target="_self">
+                <Text>{i.name}</Text>
+              </LinkOverlay>
+              {i.tags && (
+                <Stack direction="row" mt="2">
+                  {i.tags.map((i) => (
+                    <Badge key={i} variant="outline" colorScheme="green">
+                      {i}
+                    </Badge>
+                  ))}
+                </Stack>
+              )}
+            </LinkBox>
+          ))}
+        </SimpleGrid>
+      </Container>
+    </ErrorBoundary>
   );
 });
