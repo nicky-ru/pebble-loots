ae9d7396386191e95dd967bc415ec23da08d7a92
Nicky Ru <nikitkaruban@gmail.com>
2021-09-29 00:50:13 +0800

    Update devices.spec.ts

--- cypress/integration/devices.spec.ts
+++ cypress/integration/devices.spec.ts
@@ -1,9 +1,8 @@
 describe('Devices page', () => {
-  // beforeEach(() => {
-  //   cy.visit('#/devices')
-  // });
+  beforeEach(() => {
+    cy.server();
+  });
   it('should fetch devices', function() {
-    cy.server()
     cy.route(
       'GET',
       'https://protoreader.herokuapp.com/api/devices',
@@ -13,8 +12,19 @@ describe('Devices page', () => {
     cy.visit('#/devices')
     cy.get('tr')
       .should('have.length', 4);
-    
   });
+  it('should fetch devices from remote server', function() {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'https://protoreader.herokuapp.com/api/devices'
+      }
+    ).as('devices')
+    cy.visit('#/devices')
+
+    cy.get('tr')
+      .should('have.length.above', 10)
+  })
 })
 
 export {}
