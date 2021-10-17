cb483df0b5b3bb88d6b97e622c6615295ecf5ec1
Nicky Ru <nikitkaruban@gmail.com>
2021-10-17 16:54:27 +0800

    Update index.tsx

--- src/pages/DPBank/index.tsx
+++ src/pages/DPBank/index.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { observer, useLocalObservable } from 'mobx-react-lite';
-import { Container, Flex, Box, Button, Stack, Heading } from '@chakra-ui/react';
+import { Container, Flex, Box, Button, Stack, Heading, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import { useStore } from '@/store/index';
 import axios from 'axios';
 import { BooleanState } from '@/store/standard/base';
 import { RecordList } from '@/components/Records/rec-list';
+import { DPLoots } from '@/components/DPLoots';
+import { Records } from '@/components/Records';
 
 export const DPBank = observer(() => {
   const { rec, pebble, god, dpLoot } = useStore();
@@ -49,58 +51,29 @@ export const DPBank = observer(() => {
     observable.setLoading(false);
   }
 
-  async function mint(id) {
-    const snr = rec.decodedRecords[id].snr.toString();
-    const vbat = rec.decodedRecords[id].vbat.toString();
-    const latitude = rec.decodedRecords[id].latitude.toString();
-    const longitude = rec.decodedRecords[id].longitude.toString();
-    const gasResistance = rec.decodedRecords[id].gasResistance.toString();
-    const temperature = rec.decodedRecords[id].temperature.toString();
-    const pressure = rec.decodedRecords[id].pressure.toString();
-    const humidity = rec.decodedRecords[id].humidity.toString();
-    const light = rec.decodedRecords[id].light.toString();
-    const gyroscope = rec.decodedRecords[id].gyroscope.toString();
-    const accelerometer = rec.decodedRecords[id].accelerometer.toString();
-    const random = rec.decodedRecords[id].random.toString();
-
-    const dataPoint = [
-      snr, vbat, latitude, longitude, gasResistance, temperature,
-      pressure, humidity, light, gyroscope, accelerometer, random
-    ];
-
-    console.log("Minting dp", dataPoint);
-
-    try {
-      await dpLoot.contracts[god.currentChain.chainId].claim({
-        params: [god.currentNetwork.account, dataPoint]
-      })
-    } catch (e) {
-      alert(JSON.stringify(e.data.message))
-    }
-  }
-
   return(
     <Container maxW={'container.xl'} mt={16}>
       <Heading align={'center'}>Datapoint Bank</Heading>
-      <Flex mt={4}>
-        <Box w="15%">
-          <Heading size={'md'} align={'center'}>Device list</Heading>
-          <Stack mt={4}>
-            {pebble.devices?.map((device, i) => (
-              <Button
-                variant={"ghost"}
-                key={device.address}
-                onClick={() => {pebble.selectImei(device.id)}}
-              >
-                {device.name}
-              </Button>
-            ))}
-          </Stack>
-        </Box>
-        <Box flex="1">
-          <RecordList mint={mint}/>
-        </Box>
-      </Flex>
+      <Tabs>
+        <TabList>
+          <Tab>Raw</Tab>
+          <Tab>Minted</Tab>
+          <Tab>Stashed</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <Records/>
+          </TabPanel>
+          <TabPanel>
+            <DPLoots/>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+
     </Container>
   );
 });
