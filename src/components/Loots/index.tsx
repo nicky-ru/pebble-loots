8ebee6e31c59da9e27e71dd85e7e3ba2ce80141e
Nicky Ru <nikitkaruban@gmail.com>
2021-10-22 01:58:09 +0800

    Update index.tsx

--- src/components/Loots/index.tsx
+++ src/components/Loots/index.tsx
@@ -1,107 +1,64 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { observer, useLocalObservable } from 'mobx-react-lite';
-import { useDisclosure } from '@chakra-ui/react';
 import { useStore } from '@/store/index';
 import { LootCards } from './LootCards';
-import { TransactionResponse } from '@ethersproject/providers';
 import axios from 'axios';
 import { ErrorFallback } from '@/components/ErrorFallback';
 import { ErrorBoundary } from 'react-error-boundary';
 import { BooleanState } from '@/store/standard/base';
-import { TransferModal } from '@/components/Loots/TransferModal';
+import { BigNumber } from 'ethers';
 
 const IOTX_TEST_CHAINID = 4690;
 
 export const MyLoots = observer(() => {
-  const { ploot } = useStore();
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { ploot, god } = useStore();
 
   const observable = useLocalObservable(() => ({
-    tokenIds: [],
-    chainId: 0,
-    balance: 0,
-    tokenUris: [],
-    loaded: new BooleanState(),
     loading: new BooleanState(),
-    tokenToTransfer: "",
-    setChainId(newChainId: number) {
-      this.chainId = newChainId;
-    },
-    setBalance(newBalance: Partial<TransactionResponse>) {
-      // @ts-ignore
-      this.balance = newBalance.toNumber();
-    },
-    setTokenUris(newUris) {
-      this.tokenUris = newUris;
-    },
     setLoading(newLoading: boolean) {
       this.loading.setValue(newLoading);
-    },
-    setLoaded(newLoaded: boolean) {
-      this.loaded.setValue(newLoaded);
-    },
-    setTokenToTransfer(value: string) {
-      this.tokenToTransfer = value;
     }
   }))
 
   useEffect(() => {
-    if (ploot.god.currentNetwork.account) {
-      observable.setChainId(ploot.god.currentChain.chainId);
-    }
-  }, [ploot.god.currentChain.chainId]);
-
-  useEffect(() => {
-    if (observable.chainId === IOTX_TEST_CHAINID) {
+    if (god.currentChain.chainId === IOTX_TEST_CHAINID) {
       updateBalance();
     }
-  }, [ploot.god.currentNetwork.account])
+  }, [ploot.god.currentNetwork.account, god.currentChain.chainId])
 
   useEffect(() => {
-    if (observable.chainId === IOTX_TEST_CHAINID) {
-      updateBalance();
-    }
-  }, [observable.chainId])
-
-  useEffect(() => {
-    if (observable.balance) {
+    if (ploot.balance) {
       fetchLoots();
     }
-  }, [observable.balance]);
+  }, [ploot.balance]);
 
   async function fetchLoots() {
     observable.setLoading(true)
-    const tokenIds = Array(observable.balance);
+    const tokenIds = Array(ploot.balance);
 
-    for (let i = 0; i < observable.balance; i++) {
-      tokenIds[i] = await ploot.contracts[observable.chainId].tokenOfOwnerByIndex({params: [ploot.god.currentNetwork.account, i]})
+    for (let i = 0; i < ploot.balance; i++) {
+      tokenIds[i] = await ploot.contracts[god.currentChain.chainId].tokenOfOwnerByIndex({params: [ploot.god.currentNetwork.account, i]})
     }
     const tokenUris = await Promise.all(tokenIds.map(async (tid) => {
-      const uri = await ploot.contracts[observable.chainId].getTokenUri({params: [tid.toNumber()]});
+      const uri = await ploot.contracts[god.currentChain.chainId].getTokenUri({params: [tid.toNumber()]});
       return await axios.get(uri.toString());
     }))
 
-    observable.setTokenUris(tokenUris);
+    ploot.setTokenUris(tokenUris);
     observable.setLoading(false);
-    observable.setLoaded(true);
   }
 
   async function updateBalance() {
-    const balance = await ploot.contracts[observable.chainId].balanceOf({params: [ploot.god.currentNetwork.account]});
-    observable.setBalance(balance);
+    const balance = await ploot.contracts[god.currentChain.chainId].balanceOf({params: [ploot.god.currentNetwork.account]});
+    const bal = BigNumber.from(JSON.parse(JSON.stringify(balance)).hex);
+    ploot.updateBalance(bal.toNumber());
   }
 
   return(
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <LootCards
-        balance={observable.balance}
-        tokenUris={observable.tokenUris}
         loading={observable.loading}
-        loaded={observable.loaded}
-        onOpen={onOpen}
-        setTokenToTransfer={observable.setTokenToTransfer}
       />
-      <TransferModal isOpen={isOpen} onClose={onClose} tokenToTransfer={observable.tokenToTransfer}/>
     </ErrorBoundary>
   );
 });
