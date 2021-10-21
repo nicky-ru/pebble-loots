61e17579c18e110c449d5e1659a8e0dd00df5328
Nicky Ru <nikitkaruban@gmail.com>
2021-10-22 02:07:04 +0800

    Update index.tsx

--- src/components/Machinaverse/index.tsx
+++ src/components/Machinaverse/index.tsx
@@ -1,31 +1,22 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import { Container, Heading, Text, Button, Box } from '@chakra-ui/react';
+import { Box, Button, Container, Heading, Text, useDisclosure } from '@chakra-ui/react';
 import { MyLoots } from '@/components/Loots';
 
 export const Machinaverse = observer(() => {
+
   return (
-    <Container minW={'full'} align={'center'}>
+    <Container maxW={'full'} align={'Center'}>
       <Heading>
         Welcome to your Machinaverse
       </Heading>
       <Text my={4}>
-        Story about the machines
+        You are the Master.
+        There is enormous power in your hands.
+        To make "something" be "someone".
       </Text>
       <Button my={4}>
         Awake a new Pebble Tracker
-      </Button>
-      <Box borderWidth={'thin'} borderColor={'teal'} borderRadius={'3xl'} p={8} m={8}>
-        <MyLoots/>
-      </Box>
-      <Heading>
-        Vapor clouds produced
-      </Heading>
-      <Text my={4}>
-        Your Pebble produce Vapor clouds in the Machinaverse
-      </Text>
-      <Button my={4}>
-        Back to souls
       </Button>
       <Box borderWidth={'thin'} borderColor={'teal'} borderRadius={'3xl'} p={8} m={8}>
         <MyLoots/>
