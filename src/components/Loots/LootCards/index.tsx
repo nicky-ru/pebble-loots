2ae2bbfac1252cb0ff07761f33aeae8ae846568f
Nicky Ru <nikitkaruban@gmail.com>
2021-10-22 01:58:13 +0800

    Update index.tsx

--- src/components/Loots/LootCards/index.tsx
+++ src/components/Loots/LootCards/index.tsx
@@ -1,42 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { observer } from 'mobx-react-lite';
 import {
-  Container,
   Box,
-  Heading,
   Wrap,
   WrapItem,
   LinkBox,
-  Text,
   LinkOverlay,
   Image,
-  Divider,
-  Skeleton,
-  Spinner, Button,
-  Center
+  Skeleton
 } from '@chakra-ui/react';
-import { Link } from "react-router-dom";
 import { BooleanState } from '@/store/standard/base';
 import { useStore } from '@/store/index';
 
 interface PropsType {
-  balance: number;
-  tokenUris: any[];
   loading: BooleanState;
-  loaded: BooleanState;
-  onOpen: any;
-  setTokenToTransfer: any;
 }
 
 export const LootCards = observer((props: PropsType) => {
-  const { pebble } = useStore();
-  const [btnText, setBtnText] = useState("Want to mint some?");
+  const { pebble, ploot } = useStore();
 
   return(
     <Skeleton isLoaded={!props.loading.value}>
 
       <Wrap p={2} justify="center">
-        {props.tokenUris?.map((uri, i) => (
+        {ploot.tokenUris?.map((uri, i) => (
           <WrapItem key={i}>
             <LinkBox as="article"  p={5} pb={10} borderWidth="1px" rounded="md">
               <Box align={'right'} w={"200px"} h={"200px"}>
@@ -45,10 +32,6 @@ export const LootCards = observer((props: PropsType) => {
                   pebble.selectPebbleLootId(i);
                 }}/>
                 <Image src={uri.data.image}/>
-                <Button my={2} variant={'link'} onClick={() => {
-                  props.setTokenToTransfer(uri.data.name.toString().split("#")[1])
-                  props.onOpen()
-                }}>Transfer</Button>
               </Box>
             </LinkBox>
           </WrapItem>
