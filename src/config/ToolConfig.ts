54d692f3374daa200ddc1e9fb7a4b31977c22b7e
Nicky Ru <nikitkaruban@gmail.com>
2021-10-24 15:14:00 +0800

    delete dashboard

--- src/config/ToolConfig.ts
+++ src/config/ToolConfig.ts
@@ -1,7 +1,3 @@
-import { ERC20 } from '../pages/ERC20';
-import { DPBank } from '../pages/DPBank';
-import { PebbleBag } from '../pages/PebbleBag';
-
 class Tool {
   name: string;
   path: string;
@@ -13,40 +9,4 @@ class Tool {
 }
 
 export const ToolConfig = [
-  new Tool({
-    name: 'Machinaverse Passports 🪐',
-    path: '/pebble-bag',
-    component: PebbleBag,
-    tags: ['DID', 'IoT', 'Metaverse']
-  }),
-  // new Tool({
-  //   name: 'Devices',
-  //   path: '/devices',
-  //   component: Devices,
-  //   tags: ['IoT', 'Pebble']
-  // }),
-  // new Tool({
-  //   name: 'Loot gallery',
-  //   path: '/myloots',
-  //   component: MyLoots,
-  //   tags: ['NFT', 'Pebble']
-  // }),
-  // new Tool({
-  //   name: 'Mint Loot',
-  //   path: '/mintLoot',
-  //   component: Minting,
-  //   tags: ['NFT', 'Loot', 'Minting']
-  // }),
-  // new Tool({
-  //   name: 'Loot charts 🔥',
-  //   path: '/lootcharts',
-  //   component: LootCharts,
-  //   tags: ['Real data', 'Loot']
-  // }),
-  new Tool({
-    name: 'Intergalactic mine 💎',
-    path: '/datapoint-bank',
-    component: DPBank,
-    tags: ['Ore', 'PoW']
-  })
 ];
