6ed2fa9a7b500bd26f6cfd28b561bddbc1df1cbc
Nicky Ru <nikitkaruban@gmail.com>
2021-10-25 16:52:20 +0800

    Update index.tsx

--- src/components/Foundry/PlasmaInfoBox/index.tsx
+++ src/components/Foundry/PlasmaInfoBox/index.tsx
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Box, Button, Container, Stack, Text } from '@chakra-ui/react';
+import { useStore } from '@/store/index';
 
 export const PlasmaInfoBox = observer(() => {
-  const [pending, setPending] = useState<number>(0);
+  const { stash } = useStore();
+
+  useEffect(() => {
+    if (stash.userInfo?.numOfTokens) {
+      stash.updatePending();
+    }
+  }, [stash.userInfo]);
 
   return(
     <Box borderWidth={'thin'} w={'45%'} p={4} borderRadius={'md'}>
       <Stack>
         <Text align={'left'}>Your pending Plasma</Text>
-        <Text align={'left'}>{pending} PMT</Text>
+        <Text align={'left'}>{stash.pending.toNumber()} PMT</Text>
         <Stack isInline justifyContent={'space-between'} align={'center'}>
           <Button>Collect</Button>
           <Text>10$</Text>
