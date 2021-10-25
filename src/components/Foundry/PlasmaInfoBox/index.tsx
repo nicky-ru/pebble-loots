d7dfa19442739a483d676c44929792728ee4d1e4
Nicky Ru <nikitkaruban@gmail.com>
2021-10-25 18:54:39 +0800

    Update index.tsx

--- src/components/Foundry/PlasmaInfoBox/index.tsx
+++ src/components/Foundry/PlasmaInfoBox/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Box, Button, Container, Stack, Text } from '@chakra-ui/react';
 import { useStore } from '@/store/index';
+import BigNumber from 'bignumber.js';
 
 export const PlasmaInfoBox = observer(() => {
   const { stash } = useStore();
@@ -16,9 +17,13 @@ export const PlasmaInfoBox = observer(() => {
     <Box borderWidth={'thin'} w={'45%'} p={4} borderRadius={'md'}>
       <Stack>
         <Text align={'left'}>Your pending Plasma</Text>
-        <Text align={'left'}>{stash.pending.toNumber()} PMT</Text>
+        <Text align={'left'}>{
+          new BigNumber(stash.pending.toString()).div('1000000000000000000').toString()
+        } PMT</Text>
         <Stack isInline justifyContent={'space-between'} align={'center'}>
           <Button>Collect</Button>
+          <Button onClick={() => {stash.updatePending()}}>Refresh</Button>
+          <Button onClick={() => {stash.updatePool()}}>Update Pool</Button>
           <Text>10$</Text>
         </Stack>
       </Stack>
