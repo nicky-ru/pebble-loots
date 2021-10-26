8180d62f6b75ad6aeb630929070288c904b5ee10
Nicky Ru <nikitkaruban@gmail.com>
2021-10-26 17:56:33 +0800

    Update DesktopNav.tsx
    add plasma token balance view

--- src/components/Header/DesktopNav.tsx
+++ src/components/Header/DesktopNav.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Stack, BoxProps, Text, Button, Box, Img, Tag, ButtonGroup, chakra, useColorModeValue } from '@chakra-ui/react';
 import { observer, useObserver, useLocalStore } from 'mobx-react-lite';
-import { useStore } from '../../store/index';
-import { helper } from '../../lib/helper';
+import { useStore } from '@/store/index';
+import { helper } from '@/lib/helper';
 
 export const DesktopNav = observer((props: BoxProps) => {
-  const { god, lang } = useStore();
+  const { god, lang, token } = useStore();
 
   const store = useLocalStore(() => ({
     showConnecter() {
@@ -47,12 +47,41 @@ export const DesktopNav = observer((props: BoxProps) => {
       </Button>
     );
   });
+
+  const xrc20View = useObserver(() => {
+    if (!god.currentNetwork.account) {
+      return (
+        <></>
+      );
+    }
+    return (
+      <Button pr="0" pl="4" bg={useColorModeValue('gray.100', 'dark.100')}>
+        <Text mr="2" fontSize="sm">
+          <chakra.span mr={1}>
+            {
+              token.currentTokens.filter((t) => {
+                if (t.symbol === 'PBL') return true;
+              })[0].balance.format
+            }
+          </chakra.span>
+          <chakra.span mr={1}>
+            {
+              token.currentTokens.filter((t) => {
+                if (t.symbol === 'PBL') return true;
+              })[0].symbol
+            }
+          </chakra.span>
+        </Text>
+      </Button>
+    )
+  })
   return (
     <Stack direction={'row'} spacing={2} {...props}>
       <Button onClick={store.showConnecter} pl={1} borderRadius="40">
         <Img w={8} src={god.currentChain.logoUrl} />
         <Box ml={2}>{god.currentChain.name}</Box>
       </Button>
+      {xrc20View}
       {accountView}
     </Stack>
   );
