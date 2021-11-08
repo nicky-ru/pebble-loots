9402e44889b56ecfd09bd59874c6d313b34c2af9
Nicky Ru <nikitkaruban@gmail.com>
2021-11-08 22:08:12 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
+import { ColorModeScript } from "@chakra-ui/react"
+import { theme } from "@/lib/theme"
 import App from './index';
 import { Buffer } from 'buffer';
 (globalThis as any).Buffer = Buffer;
@@ -8,6 +10,7 @@ import { Buffer } from 'buffer';
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </HashRouter>
   </React.StrictMode>,
